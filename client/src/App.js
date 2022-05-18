0d4c89bff417dd6f58bde6702e030d7b835cb059
Igor Silva <osilvaigor@gmail.com>
2022-05-18 02:50:59 -0300

    feat: redesign web app

--- client/src/App.js
+++ client/src/App.js
@@ -1,124 +1,141 @@
-import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import "./App.css";
-import abi from "./utils/WavePortal.json";
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import './App.css'
+import abi from './utils/WavePortal.json'
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [miningStatus, setMiningStatus] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [miningStatus, setMiningStatus] = useState(false)
 
-  const contractAddress = "0x87A6D7Cc5136dd981535830D2DCdc07323de0082";
+  const contractAddress = '0x87A6D7Cc5136dd981535830D2DCdc07323de0082'
 
-  const contractABI = abi.abi;
+  const contractABI = abi.abi
 
   const checkIfWalletIsConnected = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
+        console.log('Make sure you have metamask!')
+        return
       } else {
-        console.log("We have the ethereum object", ethereum);
+        console.log('We have the ethereum object', ethereum)
       }
 
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
 
       if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
+        const account = accounts[0]
+        console.log('Found an authorized account:', account)
+        setCurrentAccount(account)
       } else {
-        console.log("No authorized account found");
+        console.log('No authorized account found')
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
+        alert('Get MetaMask!')
+        return
       }
 
       const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+        method: 'eth_requestAccounts',
+      })
 
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
+      console.log('Connected', accounts[0])
+      setCurrentAccount(accounts[0])
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const wave = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
         const wavePortalContract = new ethers.Contract(
           contractAddress,
           contractABI,
           signer
-        );
+        )
 
-        let count = await wavePortalContract.getTotalWaves();
-        console.log("Retrivied total wave count...", count.toNumber());
+        let count = await wavePortalContract.getTotalWaves()
+        console.log('Retrivied total wave count...', count.toNumber())
 
-        const waveTxn = await wavePortalContract.wave();
-        setMiningStatus(true);
-        console.log("Mining...", waveTxn.hash);
+        const waveTxn = await wavePortalContract.wave()
+        setMiningStatus(true)
+        console.log('Mining...', waveTxn.hash)
 
-        await waveTxn.wait();
-        setMiningStatus(false);
-        console.log("Mined --", waveTxn.hash);
+        await waveTxn.wait()
+        setMiningStatus(false)
+        console.log('Mined --', waveTxn.hash)
 
-        count = await wavePortalContract.getTotalWaves();
+        count = await wavePortalContract.getTotalWaves()
 
-        console.log("Retrieved total wave count...", count.toNumber());
+        console.log('Retrieved total wave count...', count.toNumber())
       } else {
-        console.log("Ethereum object doesn't exist!");
+        console.log("Ethereum object doesn't exist!")
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+    checkIfWalletIsConnected()
+  }, [])
 
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
+        <div className="header">✈️ Proteja-se contra voos cancelados!</div>
 
         <div className="bio">
-          I am Igor and I am learning how to make a web3 app because of my final
-          college paper. In Brazil we call it TCC.
+          Contrate seu seguro paramétrico por meio de{' '}
+          <span>smart contracts</span> baseados em <span>blockchain</span>
         </div>
-        {miningStatus && <p>Wait! The miner is working!</p>}
-        {!miningStatus && (
-          <button className="waveButton" onClick={wave}>
-            Wave at Me
+
+        {!currentAccount && (
+          <button className="buttons" onClick={connectWallet}>
+            Conecte sua carteira 😉
           </button>
         )}
-        {}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
+
+        {currentAccount && (
+          <div className="forms">
+            <form className="flightForm">
+              <div>
+                <input
+                  className="flightNumber"
+                  type="number"
+                  name="flightNumber"
+                  placeholder="número de voo"
+                />
+              </div>
+              <div>
+                <input
+                  className="buttons"
+                  type="submit"
+                  value="Pesquisar voo"
+                  onClick={wave}
+                />
+              </div>
+            </form>
+          </div>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
